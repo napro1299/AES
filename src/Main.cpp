07958fd83a7487164da3f9351b4ce25b959a432a
napro1299 <blaketadner@gmail.com>
2020-12-24 20:11:21 -0500

    tests

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,24 +3,25 @@
 #define AES128 
 #include "aes/aes.h"
 
+#include "aes/aes_test.h"
 
 int main()
 {
 
 
 
-	u8 key[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-	u8 d[16];
-	MakeRoundKey(key, d, FIRST);
-	for (int i = 0; i < 16; i++)
-		std::cout << (int)d[i] << " ";
+	////u8 key[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+	//u8 d[16];
+	//MakeRoundKey(key, d, FIRST);
+	//for (int i = 0; i < 16; i++)
+	//	std::cout << (int)d[i] << " ";
 
-	std::cout << std::endl;
+	//std::cout << std::endl;
 
-	u8 f[4] = { 0x10, 0x04, 0x08, 0x0c };
-	SubBytes<4>(f);
-	for (int i = 0; i < 4; i++)
-		std::cout << (int)f[i] << " ";
+	//u8 f[4] = { 0x10, 0x04, 0x08, 0x0c };
+	//SubBytes<4>(f);
+	//for (int i = 0; i < 4; i++)
+	//	std::cout << (int)f[i] << " ";
 
 }
 
