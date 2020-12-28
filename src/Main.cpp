c407bf60d82e915715cfbc2e24c8967001596d94
napro1299 <blaketadner@gmail.com>
2020-12-28 13:22:16 -0500

    dfd

--- src/Main.cpp
+++ src/Main.cpp
@@ -21,8 +21,7 @@ int main()
 	
 
 	uint8_t data[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-	AES_ctx ctx;
-	ctx.RoundKey = 
+	AES_ctx ctx = {};
 	AES_ECB_encrypt(&ctx, data);
 	//key_expand(key, out);
 
